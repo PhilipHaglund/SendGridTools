3a8c8df40e026fe047a45798f8e80313b398c22c
Philip Haglund <philip@gonjer.com>
2023-07-05 21:22:20 +0200

    Testing API key retrival not working

--- source/Private/Invoke-SendGrid.ps1
+++ source/Private/Invoke-SendGrid.ps1
@@ -15,6 +15,21 @@
         [Parameter()]
         [hashtable]$ContentBody
     )
+    begin {
+        function Get-UniqueProperties {
+            param (
+                [Parameter(
+                    Mandatory
+                )]
+                [object[]]$InputObject
+            )
+            $Members = foreach ($Object in $InputObject) {
+                $Object | Get-Member -MemberType NoteProperty
+            }
+            ($Members | Sort-Object -Property Name -Unique).Name | ConvertTo-TitleCase
+        }
+
+    }
     process {
         if ($PSCmdlet.ShouldProcess("$Method : $Namespace")) {
             if ($script:Session -isnot [PSSendGridSession]) {
@@ -31,29 +46,44 @@
             catch {
                 asd
             }
-            $Members = foreach ($Response in $Query) {
-                $Response | Get-Member -MemberType NoteProperty
-            }
-            $Properties = ($Members | Sort-Object -Property Name -Unique).Name | ConvertTo-TitleCase
-        
+            #$Members = foreach ($Response in $Query) {
+            #    $Response | Get-Member -MemberType NoteProperty
+            #}
+            #$Properties = ($Members | Sort-Object -Property Name -Unique).Name | ConvertTo-TitleCase
+            $Properties = Get-UniqueProperties -InputObject $Query
+
             foreach ($Object in $Query) {
                 [PSCustomObject]$PSObject = [PSCustomObject]::new()
                 foreach ($Property in $Properties) {
                     if ($Object.$Property -is [System.Management.Automation.PSCustomObject]) {
-                        $InlineMembers = $Object.$Property | Get-Member -MemberType NoteProperty
-                        $InlineProperties = ($InlineMembers | Sort-Object -Property Name -Unique).Name | ConvertTo-TitleCase
+                        #$InlineMembers = $Object.$Property | Get-Member -MemberType NoteProperty
+                        #$InlineProperties = ($InlineMembers | Sort-Object -Property Name -Unique).Name | ConvertTo-TitleCase
+                        $InlineProperties = Get-UniqueProperties -InputObject $Object.$Property
                         foreach ($InlineProperty in $InlineProperties) {
                             $PSObject | Add-Member -MemberType NoteProperty -Name ('{0}{1}' -f ($Property -replace '[\s_-]+'), $($InlineProperty -replace '[\s_-]+'))  -Value $Object.$Property.$InlineProperty
                         }
-                    
                     }
+                    
                     switch ($Object.$Property) {
                         { $_ -is [int64] -and $Property -match 'valid' } {
                             $PSObject | Add-Member -MemberType NoteProperty -Name ($Property -replace '[\s_-]+')  -Value ((Get-Date -Date '01-01-1970') + ([System.TimeSpan]::FromSeconds(($_))))
                             break
                         }
+                        { $_ -is [System.Object[]] -and $Property -match 'result' } {
+                            #$NestedProperties = Get-UniqueProperties -InputObject $Object.$Property
+                            foreach ($NestedMember in $_) {
+                                #$NestedMembers = $NestedObject | Get-Member -MemberType NoteProperty
+                                $NestedProperties = Get-UniqueProperties -InputObject $Object.$Property
+                                foreach ($NestedProperty in $NestedProperties) {
+                                    #$NestedPropertyName = ($NestedProperty.Name -replace '[\s_-]+') | ConvertTo-TitleCase
+                                    #$NestedPropertyValue = $NestedObject.$($NestedMember.Name)
+                                    $PSObject | Add-Member -MemberType NoteProperty -Name ($NestedProperty -replace '[\s_-]+') -Value ($NestedMember.$NestedProperty) -Force
+                                }
+                            }
+                            break
+                        }
                         Default {
-                            $PSObject | Add-Member -MemberType NoteProperty -Name ($Property -replace '[\s_-]+')  -Value $Object.$Property -Force
+                            $PSObject | Add-Member -MemberType NoteProperty -Name ($Property -replace '[\s_-]+') -Value $Object.$Property -Force
                             break
                         }
                     }
--- source/Public/Set-SGApiKey.ps1
+++ source/Public/Set-SGApiKey.ps1
@@ -16,12 +16,12 @@
         Specifies the new name of the API Key. This parameter is not mandatory.
 
     .EXAMPLE
-        PS C:\> Set-SGApiKey -UniqueId 'SG.aBcDeFg123.456hIjKlMnOpQr' -Scopes 'access_settings.activity.read', 'alerts.create', 'alerts.read'
+        PS C:\> Set-SGApiKey -UniqueId 'R2l2W3kZSQukQv4lCkG3zW' -Scopes 'access_settings.activity.read', 'alerts.create', 'alerts.read'
 
         This command updates the scopes of the API Key with the specified ID within the current SendGrid instance.
 
     .EXAMPLE
-        PS C:\> Set-SGApiKey -UniqueId 'SG.aBcDeFg123.456hIjKlMnOpQr' -Scopes 'access_settings.activity.read', 'alerts.create', 'alerts.read' -NewName 'MyUpdatedKey'
+        PS C:\> Set-SGApiKey -UniqueId 'R2l2W3kZSQukQv4lCkG3zW' -Scopes 'access_settings.activity.read', 'alerts.create', 'alerts.read' -NewName 'MyUpdatedKey'
 
         This command updates both the name and scopes of the API Key with the specified ID within the current SendGrid instance.
     #>
@@ -51,7 +51,7 @@
         $SGApiKey = Get-SGApiKey -UniqueId $UniqueId -ErrorAction Stop
         if ($PSCmdlet.ShouldProcess($UniqueId)) {
             Write-Verbose -Message ('Updating key {0}' -f $SGApiKey.Name)
-            
+
             [hashtable]$ContentBody = @{
                 scopes = $Scopes
             }
