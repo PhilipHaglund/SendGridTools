54ebef6fc798b23456ca0be0df8a88584c7b097e
Philip Haglund <gonjer@gmail.com>
2024-01-05 16:16:54 +0100

    INital Commit

---
+++ source/Classes/SGCustom.ps1
@@ -0,0 +1,45 @@
+﻿class UnixTime {
+    [datetime]$DateTime
+    [int]$UnixTimestamp
+
+    static [datetime]EpochStart() {
+        return ([datetime]::new(1970, 1, 1, 0, 0, 0, ([DateTimeKind]::Utc)))
+    }
+
+    
+    UnixTime () {
+
+    }
+    UnixTime([datetime]$DateTime) {
+        $this.DateTime = $DateTime
+        $this.UnixTimestamp = $this.ToUnixTime()
+        #$EpochStart = [datetime]::new(1970, 1, 1, 0, 0, 0, ([DateTimeKind]::Utc))
+        #$EpochStart = [UnixTime]::EpochStart()
+        #$this.UnixTimestamp = [int]([datetime]$DateTime - $EpochStart).TotalSeconds
+        
+    }
+
+    UnixTime([int]$UnixTimestamp) {
+        $this.UnixTimestamp = $UnixTimestamp
+        $this.DateTime = $this.ToDateTime()
+    }
+
+    [datetime] ToDateTime() {
+        return [datetime]::new(1970, 1, 1, 0, 0, 0, 0).AddSeconds($this.UnixTimestamp)
+    }
+
+    [datetime] ToDateTimeUTC() {
+        return [datetime]::new(1970, 1, 1, 0, 0, 0, 0, [DateTimeKind]::Utc).AddSeconds($this.UnixTimestamp)
+    }
+
+    [UnixTime] ToUnixTime() {
+        $EpochStart = [UnixTime]::EpochStart()
+        Write-Verbose $EpochStart -Verbose
+        Write-Verbose ([int]([datetime]$this.DateTime - $EpochStart).TotalSeconds) -Verbose
+        return [int]([datetime]$this.DateTime - $EpochStart).TotalSeconds
+    }
+
+    [string] ToString() {
+        return $this.UnixTimestamp.ToString()
+    }
+}
---
+++ source/Private/ConvertTo-SendGridAddress.ps1
@@ -0,0 +1,33 @@
+﻿function ConvertTo-SendGridAddress {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            Position = 0
+        )]
+        [Alias("EmailAddress")]
+        [object[]]$Address
+    )
+    process {
+        foreach ($A in $Address) {
+            try {
+                $EmailAddress = [System.Net.Mail.MailAddress]$A
+                if ($null -eq $EmailAddress.DisplayName -or $EmailAddress.DisplayName -eq [string]::Empty) {
+                    @{
+                        email = $EmailAddress.Address
+                    }
+                }
+                else {
+                    @{
+                        email = $EmailAddress.Address
+                        name  = $EmailAddress.DisplayName
+                    }
+                }
+            }
+            catch {
+                Write-Error "Invalid email address: $A" -ErrorAction Stop
+            }
+        }
+    }
+}
