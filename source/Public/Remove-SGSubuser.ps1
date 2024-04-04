61748045e795092e80c84851a3e88b97f8567345
Philip Haglund <gonjer@gmail.com>
2024-04-04 16:07:50 +0200

    Inital commit

---
+++ source/Public/Remove-SGSubuser.ps1
@@ -0,0 +1,48 @@
+﻿function Remove-SGSubuser {
+    <#
+    .SYNOPSIS
+        Removes a specific Subuser within the current SendGrid instance.
+
+    .DESCRIPTION
+        Remove-SGSubuser removes a specific Subuser based on its username within the current SendGrid instance.
+
+    .PARAMETER Username
+        Specifies the ID of the Subuser to remove.
+
+    .EXAMPLE
+        PS C:\> Remove-SGSubuser -Username <username>
+        
+        This command removes the Subuser with the specified username within the current SendGrid instance.
+    #>
+    [CmdletBinding(
+        SupportsShouldProcess,
+        ConfirmImpact = 'High'
+    )]
+    param (
+        # Specifies the ID of the Subuser to remove.
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName
+        )]
+        [string]$Username
+    )
+
+    process {
+        $InvokeSplat = @{
+            Method        = 'Delete'
+            Namespace     = "subusers/$Username"
+            ErrorAction   = 'Stop'
+            CallingCmdlet = $PSCmdlet.MyInvocation.MyCommand.Name
+        }
+
+        if ($PSCmdlet.ShouldProcess(('{0}' -f 'Subuser'))) {
+            try {
+                Invoke-SendGrid @InvokeSplat
+            }
+            catch {
+                Write-Error ('Failed to remove SendGrid Subuser. {0}' -f $_.Exception.Message) -ErrorAction Stop
+            }
+        }
+    }
+}
